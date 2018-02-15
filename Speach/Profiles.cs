357ddc4c94ddb416a87d998b7b8b582edaed6232
VeyMess <rak200@mail.ru>
2018-02-15 18:58:23 +0400

    ->>

--- Speach/Profiles.cs
+++ Speach/Profiles.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Xml;
+using System.Windows.Forms;
 using System.Linq;
 using System.Text;
 
@@ -10,12 +11,50 @@ namespace Speach
     {
         private XmlDocument doc;
         private List<Rule> rules;
+        private string name;
 
-        private Dictionary<string, Rule> wordRule;
+        private Dictionary<string,Rule> wordRule;
 
         private void OpenXmlProf()
         {
+            doc.Load(Application.StartupPath + "\\Profiles\\" + name + ".spprof");
 
+            XmlElement root = doc.DocumentElement;
+            foreach(XmlNode rule in root.ChildNodes)
+            {
+                if(rule.Name=="rule")
+                {
+                    Rule rnd = new Rule(rule.Attributes["id"].Value);
+
+                    foreach(XmlNode rNode in rule.ChildNodes)
+                    {
+                        if(rNode.Name=="dictionary")
+                        {
+                            foreach(XmlNode wNode in rNode.ChildNodes)
+                            {
+                                if(wNode.Name=="word")
+                                {
+                                    rnd.AddWord(wNode.InnerText); 
+                                }
+                            }
+                        }
+                        else if(rNode.Name=="command")
+                        {
+                            rnd.Command = rNode.InnerText;
+                        }
+                    }
+
+                    rules.Add(rnd);
+                }
+            }
+
+            foreach(Rule rnd in rules)
+            {
+                foreach(string word in rnd)
+                {
+                    wordRule.Add(word,rnd);
+                }
+            }
         }
     }
 }
--- Speach/Rule.cs
+++ Speach/Rule.cs
@@ -1,14 +1,45 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Speach
 {
-    class Rule
+    class Rule : IEnumerable
     {
-        string[] words;
+        List<string> words;
         string command;
         string name;
+
+        public Rule(string n)
+        {
+            this.name = n;
+            command = null;
+            words = new List<string>();
+        }
+
+        public Rule()
+        {
+            name = null;
+            command = null;
+            words = new List<string>();
+        }
+
+        public void AddWord(string wrd)
+        {
+            words.Add(wrd);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)words).GetEnumerator();
+        }
+
+        public string Command
+        {
+            set { command = value; }
+            get { return command; }
+        }
     }
 }
